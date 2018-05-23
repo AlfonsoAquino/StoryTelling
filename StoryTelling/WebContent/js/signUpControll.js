aa6e10fc967a22cc5e91c2385dd332b58313a0d5
alfonso <alfonso@alfonso-SATELLITE-L50D-B>
2018-05-23 11:41:52 +0200

    mancava sto file al commit di prima

---
+++ StoryTelling/WebContent/js/signUpControll.js
@@ -0,0 +1,65 @@
+/**
+ * 
+ */
+$(document).ready(function() {
+	// $("#quickstart-sign-up").click(function(){
+	// var pass=$("#password").val();
+	// var email=$("#email").val();
+	//		
+	// if(!isEmail(email)){
+	// var thisAlert = $("#email").parent();
+	// $(thisAlert).addClass('alert-validate');
+	// }
+	//
+	// });
+
+	var input = $('.validate-input .input100');
+
+	$('.contact100-form-btn').click(function() {
+		var check = true;
+
+		for (var i = 0; i < input.length; i++) {
+			if (validate(input[i]) == false) {
+				showValidate(input[i]);
+				check = false;
+			}
+		}
+		return check;
+		
+	});
+
+	$('.validate-form .input100').each(function() {
+		$(this).focus(function() {
+			hideValidate(this);
+		});
+	});
+
+	function validate (input) {
+        if($(input).attr('type') == 'email' || $(input).attr('name') == 'email') {
+            if($(input).val().trim().match(/^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{1,5}|[0-9]{1,3})(\]?)$/) == null) {
+                return false;
+            }
+        }else if($(input).attr('type') == 'password' || $(input).attr('name') == 'password'){
+        	if($(input).val().trim().match(/^([0-9a-zA-Z]{6,})+$/) == null) 
+                return false;
+        }
+        else {
+            if($(input).val().trim() == ''){
+                return false;
+            }
+        }
+        }
+	
+	
+	function showValidate(input) {
+		var thisAlert = $(input).parent();
+
+		$(thisAlert).addClass('alert-validate');
+	}
+
+	function hideValidate(input) {
+		var thisAlert = $(input).parent();
+
+		$(thisAlert).removeClass('alert-validate');
+	}
+});
